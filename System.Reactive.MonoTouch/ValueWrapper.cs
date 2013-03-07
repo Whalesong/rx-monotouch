c39bd8c63f5d984435e65c79478a0d60e144d380
Ryan Hughes <rynhughes@gmail.com>
2013-03-07 16:21:41 -0600

    not sure

--- System.Reactive.MonoTouch/ValueWrapper.cs
+++ System.Reactive.MonoTouch/ValueWrapper.cs
@@ -2,7 +2,46 @@ using System;
 
 namespace System.Reactive.MonoTouch
 {
-    class ValueWrapper<T> : IComparable<ValueWrapper<T>>, IEquatable<ValueWrapper<T>>
+	public class StructWrapper<T> : IEquatable<StructWrapper<T>>
+		where T : struct, IEquatable<T>
+	{
+		static public implicit operator StructWrapper<T> (T value) 
+		{
+			return new StructWrapper<T> (value);
+		}
+		
+		static public implicit operator T (StructWrapper<T> wrapper)
+		{
+			return wrapper.Value;
+		}
+		
+		public T Value { get; private set; }
+		
+		public StructWrapper (T value)
+		{
+			Value = value;
+		}
+		
+		public bool Equals (StructWrapper<T> that)
+		{
+			return this.Value.Equals (that.Value);
+		}
+		
+		public override bool Equals (object obj)
+		{
+			var that = obj as StructWrapper<T>;
+			if (that == null)
+				return false;
+			
+			return this.Equals (that);
+		}
+		
+		public override int GetHashCode ()
+		{
+			return Value.GetHashCode ();
+		}
+	}
+    public class ValueWrapper<T> : IComparable<ValueWrapper<T>>, IEquatable<ValueWrapper<T>>
         where T : struct, IComparable<T>, IEquatable<T>
     {
         static public implicit operator ValueWrapper<T> (T value) 
